f2c971a14e2ce4e0b56a78700618910086ead4c0
Amin <Hosseini.ma.71@gmail.com>
2020-09-06 10:22:43 +0430

    Make Ping mechanism customizable in NetworkChangeReceiver

---
+++ commontools/src/main/java/com/paraxco/commontools/BroadCastReceiver/NetworkChangeReceiver.kt
@@ -0,0 +1,92 @@
+package com.paraxco.commontools.BroadCastReceiver
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.os.Build
+import android.os.Handler
+import com.paraxco.commontools.Observers.NetworkObserverHandler
+import com.paraxco.commontools.Observers.NetworkStateLiveData
+import com.paraxco.commontools.Observers.NetworkStateLiveData.NetworkState
+import com.paraxco.commontools.Utils.Utils
+import java.util.concurrent.atomic.AtomicBoolean
+
+/**
+ *
+ */
+class NetworkChangeReceiver : BroadcastReceiver() {
+    companion object {
+        var PingBeforeInform = false
+        var pingHost = "www.google.com"
+        var pingTimeOut = 1000
+        var pingMechanism:((host:String,timeout:Int)->Boolean)?=null
+    }
+
+    var isRegistered = AtomicBoolean(false)
+    private var lastState: Boolean? = null
+    private var pingThread: Thread? = null
+
+    override fun onReceive(context: Context, intent: Intent) {
+        if (intent.extras != null) {
+            val currentState = Utils.isNetworkAvailable(context)
+            //to prevent multiple call from device
+            if (lastState == null || lastState != currentState) {
+                lastState = currentState
+                changeState(currentState)
+            }
+        }
+    }
+
+    private fun changeState(currentState: Boolean) {
+        if (!currentState || !PingBeforeInform) informObservers(currentState) else checkNetworkStateByPing()
+    }
+
+    private fun checkNetworkStateByPing() {
+        val handler = Handler()
+        if (pingThread != null) pingThread!!.stop()
+        pingThread = Thread {
+            val isReachable =pingMechanism?.run { pingMechanism?.invoke(pingHost,pingTimeOut) }?: Utils.isConnectedToThisServer(pingHost, pingTimeOut)
+            handler.post { if (isReachable) informObservers(true) else informObservers(false) }
+        }
+        pingThread!!.start()
+    }
+
+    private fun informObservers(currentState: Boolean) {
+        NetworkObserverHandler.getInstance().informObservers(currentState)
+        NetworkStateLiveData.getInstance().postValue(NetworkState(currentState))
+    }
+
+    fun registerService(context: Context) {
+        if (!isRegistered.get()) if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            if (getObserversCount() == 1)//for the first time
+            try {
+                context.registerReceiver(this,
+                        IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+                isRegistered.set(true)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun unRegisterService(context: Context) {
+        if (isRegistered.get()) if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            if (getObserversCount() == 0 && lastCont == 1)//for the last time
+            try {
+                context.unregisterReceiver(this)
+                isRegistered.set(false)
+            } catch (e: IllegalArgumentException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    fun refreshCurrentState(context: Context?) {
+        val currentState = Utils.isNetworkAvailable(context)
+        changeState(currentState)
+    }
+
+
+}
